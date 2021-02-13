9da7c3602332136930ea8a8adfadfb56c0d9bcd3
Nico <nicolas1.graveleau@epitech.eu>
2021-02-14 00:24:07 +0100

    better gravity

--- player.lua
+++ player.lua
@@ -3,13 +3,15 @@ Player = Entity:extend()
 function Player:new(x, y)
   Player.super.new(self, x, y, "res/monkeystop.png", 1.5625)
   self.speed = 250
-  self.gravity = 350
+  self.gravity = 400
   self.jumpVelocity = 0;
   self.isOnFloor = false
 end
 
 function Player:update(dt, world)
   local dx, dy = 0, 0
+  local tx, ty
+  
 
   if love.keyboard.isDown("right")
   then
@@ -19,23 +21,23 @@ function Player:update(dt, world)
     dx = -self.speed * dt
   end
   
-  if self.isOnFloor
+  
+  tx, ty = world:check(self, self.x, self.y - 0.001); -- check if head touch smth
+  
+  if self.isOnFloor or ty == self.y
   then
     self.jumpVelocity = 0
   end
   
   if love.keyboard.isDown("up") and self.isOnFloor
   then
-    self.jumpVelocity = -250
+    self.jumpVelocity = -300
   end
 
   if (not self.isOnFloor) or (self.jumpVelocity ~= 0) 
   then
-    if not self.isOnFloor then print("floor") end
-    if self.jumpVelocity ~= 0 then print("jump") end
-    print(self.jumpVelocity)
-    dy = self.jumpVelocity * dt
     self.jumpVelocity = self.jumpVelocity + self.gravity * dt
+    dy = self.jumpVelocity * dt
   end
   
 --  if not self.isOnFloor
@@ -49,8 +51,19 @@ function Player:update(dt, world)
     if newY == self.y
     then
       self.isOnFloor = true
+    else
+      self.isOnFloor = false
     end
     self.y = newY
     self.x = newX
   end
+  tx, ty = world:check(self, self.x, self.y + 0.001); -- check if on floor
+  if ty == self.y
+  then
+  	self.isOnFloor = true
+  else
+	  self.isOnFloor = false
+  end
+  print(dy)
+  print(self.isOnFloor)
 end
